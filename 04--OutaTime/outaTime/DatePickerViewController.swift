a05e573e22b3c27ddc53abb35fb5357692a49bfc
daaavid <david.j.c.johnson@gmail.com>
2015-10-09 10:43:15 -0400

    Got NSDate and NSDateFormatter working and implemented

---
+++ 04--OutaTime/outaTime/DatePickerViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DatePickerViewController.swift
+//  outaTime
+//
+//  Created by Ennui on 10/8/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
+{
+    
+    @IBOutlet var picker: UIDatePicker!
+    var delegate: DatePickerDelegate?
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillDisappear(animated: Bool) //function already exists in parent, so we must override it
+    {
+        //runs as view is about to disappear
+        //viewDidDisappear after it's gone
+        //viewDidAppear, viewWillAppear etc
+        
+//        delegate?.timerWasChosen(60-picker.s) //asking picker to give us selected row in selected (0) component
+        super.viewWillDisappear(animated) //still runs viewWillDisappear, but we're going to add custom commands after
+    }
+
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int
+    {
+        //how many wheels
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int //must return Int
+    {
+        //how many rows
+        //        return 10 //creates 10 rows, starting from 0
+        //        return cities.count //creates 2 rows, counting from cities array
+        return 60
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? //must return String
+    {
+        //        return "\(cities[row])"
+        //        return "\(cities[1])"
+        //        return "\(row + 1)"
+        return "\(60 - row)"
+    }
+    
+}
--- 04--OutaTime/outaTime/outaTime/TimeCircuitsViewController.swift
+++ 04--OutaTime/outaTime/outaTime/TimeCircuitsViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class TimeCircuitsViewController: UIViewController {
+@objc protocol DatePickerDelegate //need this for class that's /recieving/ the data from timerpickerdelegate
+{
+    func timerWasChosen(timerCount: Int)
+    //as back button is tapped, and view is transitioning back to main screen, return time
+}
 
-    override func viewDidLoad() {
+class TimeCircuitsViewController: UIViewController, DatePickerDelegate
+{
+    @IBOutlet var destTime: UILabel!
+    @IBOutlet var presTime: UILabel!
+    @IBOutlet var lastTimeDep: UILabel!
+    @IBOutlet var speed: UILabel!
+//    @IBOutlet var travBack: UIButton!
+    
+    let formatter = NSDateFormatter()
+    
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        formatter.dateFormat = NSDateFormatter.dateFormatFromTemplate("MMM dd YYYY", options: 0, locale: NSLocale(localeIdentifier: "en-US"))
+        presTime.text = formatter.stringFromDate(NSDate()).uppercaseString
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        if segue.identifier == "ShowDatePickerSegue"
+        {
+            //storing string that connects to balloon
+            let datePickerVC = segue.destinationViewController as! DatePickerViewController //view controller that this segue points at. in this case, the timerpickerviewcontroller
+            
+            datePickerVC.delegate = self
+            //self is referring to the countdownviewcontroller object
+            //at the top, we declared that we were the timerpickerviewcontroller delegate
+            
+        }
+    }
+    
+    func timerWasChosen(timerCount: Int)
+    {
+//        destTime.text = "\(timerCount)"
+    }
+    
+    
 
 }
 
