66ff7a3c5ba44a208e9b9415c1e47422a1c8d81e
daaavid <david.j.c.johnson@gmail.com>
2015-10-11 01:07:09 -0400

    Finished second challenge

--- Coding Kata/KataWk1.playground/Contents.swift
+++ Coding Kata/KataWk1.playground/Contents.swift
@@ -15,12 +15,82 @@ for num in multiples
 print(totalSum)
 
 //Question 2
-//var palin1 = 99
-//var palin2 = 91
-//let palinProd = palin1 * palin2
-//
-//func checkPalin(palinProd: Int) -> Int
+
+//let testPalinProd = 906609
+//var testPalinProdAfter = Int(String(testPalinProd).characters.count)
+//if testPalinProdAfter % 2 == 0
 //{
+//    print("Success")
+//}
+
+
+
+func checkPalin(palinProd: Int) -> Bool
+{
+    var isPalin = false
+//    var revArray: Array<Character> = []
+//    let charArray = String(Array(String(palinProd).characters).reverse())
+    
+//    let charArray = Array(String(palinProd).characters)
+    let charArrayReversed = Array(String(palinProd).characters.reverse())
+//    if charArray[0] == charArrayReversed[0]
+//    {
+        if String(charArrayReversed) == String(palinProd)
+        {
+           isPalin = true
+        }
+//    }
+    
+    return isPalin
+}
+
+checkPalin(900009)
+
+//func checkIfOdd(palinProdStrSav: String) -> Bool
+//{
+//    if palinProdStrSav
+//    
+//    return true
+//}
+
+//checkPalin(palinProd)
+
+var palinProd = 0
+var lgCurrPalin = 0
+
+for var palinMul1 = 999; palinMul1 >= 100; palinMul1--
+{
+    for var palinMul2 = 999; palinMul2 >= 100; palinMul2--
+    {
+        palinProd = palinMul1 * palinMul2
+//        var checkEvenOdd = Int(String(palinProd).characters.count)
+//        if checkEvenOdd % 2 == 0
+//        {
+            if checkPalin(palinProd) == true
+            {
+                if palinProd > lgCurrPalin
+                {
+                    lgCurrPalin = palinProd
+                }
+                else
+                {
+                    break
+                }
+//            }
+        }
+    }
+}
+print(lgCurrPalin)
+
+
+
+
+//var palin1 = 99
+////var palin2 = 91
+////let palinProd = palin1 * palin2
+//
+////func checkPalin(prod) -> Int
+////{
 //    var revArray: Array<Character> = []
 //    let palinProdStr = String(palinProd)
 //    let characters = Array(palinProdStr.characters)
@@ -28,8 +98,8 @@ print(totalSum)
 //    {
 //        revArray.append(ch)
 //    }
-//    return(revArray)
-//}
+////    return(revArray)
+////}
 //String(revArray)
 //
 //if String(revArray) == String(palinProd)
@@ -40,15 +110,13 @@ print(totalSum)
 //{
 //    print("not equal")
 //}
-
-class PalinCheck
-{
-    
-    
-    init()
-    {
-        
-    }
-    
-    func
-}
+//
+//class PalinCheck
+//{
+//    
+//    init()
+//    {
+//        
+//    }
+//
+//}
