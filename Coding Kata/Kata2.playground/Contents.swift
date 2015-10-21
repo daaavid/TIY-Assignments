8c09321fbdb1063052688a3956b66051865346da
daaavid <david.j.c.johnson@gmail.com>
2015-10-20 21:09:07 -0400

    Finished Q2

--- Coding Kata/Kata2.playground/Contents.swift
+++ Coding Kata/Kata2.playground/Contents.swift
@@ -20,6 +20,7 @@ func checkLeapYear(year: Int) -> Bool
 
 checkLeapYear(2012)
 checkLeapYear(2016)
+checkLeapYear(2020)
 checkLeapYear(1900)
 checkLeapYear(2000)
 //: ## Question 2
@@ -27,21 +28,36 @@ checkLeapYear(2000)
 //: You are working on a smart-fridge. The smart-fridge knows how old the eggs and bacon in it are. You know that eggs spoils after 3 weeks (21 days) and bacon after one week (7 days). Given ```baconAge``` and ```eggsAge``` (in days) determine if you can cook bacon and eggs or what ingredients you need to throw out.
 //:
 //: If you can cook bacon and eggs print you can cook bacon and eggs. If you need to throw out any ingredients for each one print a line with the text throw out ingredient (throw out bacon or throw out eggs) in any order.
-var baconAge = 0
 var eggsAge = 0
+var baconAge = 0
 
-if baconAge <= 7 || eggsAge <= 21
+func breakfastTimeAllTheTime(eggsAge: Int, baconAge: Int) -> String
 {
-    if baconAge <= 7
-    {
-        print("bacon")
-    }
+    var breakfastString = "You can have cake for breakfast"
     
+    if baconAge <= 7 && eggsAge <= 21
+    {
+        breakfastString = "You can have bacon and eggs."
+    }
+    else if baconAge <= 7
+    {
+        breakfastString = "You can have bacon, but throw out the eggs."
+    }
+    else if eggsAge <= 21
+    {
+        breakfastString = "You can have eggs, but throw out the bacon."
+    }
+    else
+    {
+        breakfastString = "Throw out your fridge; it's all bad"
+    }
+    return breakfastString
 }
-else
-{
-    print("no")
-}
+
+print(breakfastTimeAllTheTime(1, baconAge: 1))
+print(breakfastTimeAllTheTime(1, baconAge: 30))
+print(breakfastTimeAllTheTime(30, baconAge: 1))
+print(breakfastTimeAllTheTime(10000, baconAge: 10000))
 
 //: ## Question 3
 //: ### Difference of Square of Sums and Sum of Squares
