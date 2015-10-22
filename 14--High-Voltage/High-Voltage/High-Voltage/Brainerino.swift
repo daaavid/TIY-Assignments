a8c899a9d091d6714290fb5e49b4fa03fa4caa20
daaavid <david.j.c.johnson@gmail.com>
2015-10-22 17:29:53 -0400

    App is functioning. Need to polish it up

---
+++ 14--High-Voltage/High-Voltage/High-Voltage/Brainerino.swift
@@ -0,0 +1,126 @@
+//
+//  CalculatorBrainerino.swift
+//  High-Voltage
+//
+//  Created by david on 10/22/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Foundation
+
+class Brainerino
+{
+    var watts: Float = 0.0
+    var volts: Float  = 0.0
+    var amps: Float  = 0.0
+    var ohms: Float  = 0.0
+    var calculateFinished = false
+    
+    func calculate()
+    {
+        if watts != 0 && amps != 0
+        {
+            ohms = ohmsCalc(3)
+            volts = voltsCalc(2)
+        }
+        else if watts != 0 && volts != 0
+        {
+            ohms = ohmsCalc(2)
+            amps = ampsCalc(2)
+        }
+        else if watts != 0 && ohms != 0
+        {
+            amps = ampsCalc(2)
+            volts = voltsCalc(3)
+        }
+        else if volts != 0 && amps != 0
+        {
+            ohms = ohmsCalc(1)
+            watts = wattsCalc(1)
+        }
+        else if volts != 0 && ohms != 0
+        {
+            watts = wattsCalc(2)
+            amps = ampsCalc(1)
+        }
+        else if ohms != 0 && amps != 0
+        {
+            watts = wattsCalc(3)
+            volts = (1)
+        }
+        
+        calculateFinished = true
+    }
+    
+    func ohmsCalc(cv: Int) -> Float
+    {
+        switch cv
+        {
+        case 1:
+            ohms = volts / amps
+        case 2:
+            ohms = (volts * volts) / watts
+        case 3:
+            ohms = watts / (amps * amps)
+        default:
+            ohms = 1000000
+        }
+        return ohms
+    }
+    
+    func ampsCalc(cv: Int) -> Float
+    {
+        switch cv
+        {
+        case 1:
+            amps = volts / ohms
+        case 2:
+            amps = watts / volts
+        case 3:
+            amps = Float(sqrt(Double(watts) / Double(ohms)))
+        default:
+            amps = 1000000
+        }
+        return amps
+    }
+    
+    func voltsCalc(cv: Int) -> Float
+    {
+        switch cv
+        {
+        case 1:
+            volts = amps * ohms
+        case 2:
+            volts = watts / amps
+        case 3:
+            volts = Float(sqrt(Double(watts) * Double(ohms)))
+        default:
+            volts = 1000000
+        }
+        return volts
+    }
+    
+    func wattsCalc(cv: Int) -> Float
+    {
+        switch cv
+        {
+        case 1:
+            watts = volts * amps
+        case 2:
+            watts = (volts * volts) / ohms
+        case 3:
+            watts = (amps * amps) * ohms
+        default:
+            watts = 1000000
+        }
+        return watts
+    }
+    
+    func brainReset()
+    {
+        watts = 0
+        volts = 0
+        amps = 0
+        ohms = 0
+    }
+}
--- 14--High-Voltage/High-Voltage/High-Voltage/CalcTableViewController.swift
+++ 14--High-Voltage/High-Voltage/High-Voltage/CalcTableViewController.swift
@@ -13,21 +13,34 @@ protocol ElecPopoverTableViewControllerDelegate
     func elecItemWasChosenFromPopover(chosenItem: String)
 }
 
-class CalcTableViewController: UITableViewController, ElecPopoverTableViewControllerDelegate, UIPopoverPresentationControllerDelegate
+class CalcTableViewController: UITableViewController, ElecPopoverTableViewControllerDelegate, UIPopoverPresentationControllerDelegate, UITextFieldDelegate
 {
     @IBOutlet weak var addButton: UIBarButtonItem!
     @IBOutlet weak var clearButton: UIBarButtonItem!
+    @IBOutlet weak var calculateButton: UIButton!
+
+    var calcButtonEnabled = false
+    
     var shownCalcItems = Array<String>()
     
-    var calcBrain = CalculatorBrainerino?()
+    var brain = Brainerino()
     
-    let allCalcItems = ["Yup", "Nope", "Maybe"]
-    var remainingCalcItems = ["Yup", "Nope", "Maybe"]
+    let allCalcItems = ["AMPS", "WATTS", "VOLTS", "OHMS"]
+    var remainingCalcItems = ["AMPS", "WATTS", "VOLTS", "OHMS"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "yup mhm"
+        title = "electricity and also more electricity"
+        
+        if calcButtonEnabled == false
+        {
+            calculateButton.enabled = false
+        }
+        else
+        {
+            calculateButton.enabled = true
+        }
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -56,11 +69,42 @@ class CalcTableViewController: UITableViewController, ElecPopoverTableViewContro
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("CalcCell", forIndexPath: indexPath) as! CalcCell
+        
+
 
         let aCalc = shownCalcItems[indexPath.row]
         cell.calcLabel.text = aCalc
-        cell.calcNumTextField.text = "1+1"
-
+        
+        if cell.calcNumTextField.text == ""
+        {
+            cell.calcNumTextField.becomeFirstResponder()
+        }
+    
+        if brain.calculateFinished == true
+        {
+            
+            if cell.calcLabel.text == "WATTS"
+            {
+                print("watts")
+                cell.calcNumTextField.text = String(brain.watts)
+            }
+            if cell.calcLabel.text == "VOLTS"
+            {
+                print("volts")
+                cell.calcNumTextField.text = String(brain.volts)
+            }
+            if cell.calcLabel.text == "AMPS"
+            {
+                print("amps")
+                cell.calcNumTextField.text = String(brain.amps)
+            }
+            if cell.calcLabel.text == "OHMS"
+            {
+                cell.calcNumTextField.text = String(brain.ohms)
+            }
+            
+        }
+        
         return cell
     }
     
@@ -72,13 +116,59 @@ class CalcTableViewController: UITableViewController, ElecPopoverTableViewContro
         let elecItemToRemoveFromPopover = (remainingCalcItems as NSArray).indexOfObject(chosenItem)
         remainingCalcItems.removeAtIndex(elecItemToRemoveFromPopover)
         
-        if remainingCalcItems.count == 0
-        {
-            addButton.enabled = false
-        }
-        
         tableView.reloadData()
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        var rc = false
+        
+        if textField.text != ""
+        {
+            rc = true
+            let contentView = textField.superview
+            let cell = contentView?.superview as! CalcCell
+//            let indexPath = tableView.indexPathForCell(cell)
+//            let aCalcItem = allCalcItems[indexPath!.row]
+
+            checkCalcLabel(cell.calcLabel.text!, textField: cell.calcNumTextField.text!)
+            
+            textField.resignFirstResponder()
+            
+            if remainingCalcItems.count == 2
+            {
+                addButton.enabled = false
+                calcButtonEnabled == true
+            }
+        }
+        
+        return rc
+    }
+    
+    func checkCalcLabel(label: String, textField: String)
+    {
+        if label == "WATTS"
+        {
+            print("watts")
+            brain.watts = Float(textField)!
+        }
+        if label == "VOLTS"
+        {
+            print("volts")
+            brain.volts = Float(textField)!
+        }
+        if label == "AMPS"
+        {
+            print("amps")
+            brain.amps = Float(textField)!
+        }
+        if label == "OHMS"
+        {
+            print("ohms")
+            brain.watts = Float(textField)!
+        }
+    }
+
     
     // MARK: - UIPopoverPresentationController Delegate
     
@@ -88,6 +178,23 @@ class CalcTableViewController: UITableViewController, ElecPopoverTableViewContro
         return .None
     }
 
+    @IBAction func calculateButton(sender: UIButton)
+    {
+        brain.calculate()
+        
+        print(" watts \(brain.watts) \n volts \(brain.volts) \n amps \(brain.amps) \n ohms \(brain.ohms)")
+        
+        shownCalcItems.removeAll(); for x in allCalcItems
+        {
+            shownCalcItems.append(x)
+        }
+        
+        calcButtonEnabled = false
+        tableView.reloadData()
+        
+        spookyScarySkeleton()
+
+    }
     
 
     /*
@@ -144,11 +251,11 @@ class CalcTableViewController: UITableViewController, ElecPopoverTableViewContro
     }
 
     
-    // MARK: - THE ACTION HANDLERS 4: RETURN OF THE ACTION
+    // MARK: - THE ACTION HANDLERS 4: FALL OF THE HANDLERS
     
     @IBAction func addButton(sender: UIBarButtonItem)
     {
-        
+
     }
     
     @IBAction func clearButton(sender: UIBarButtonItem)
@@ -159,6 +266,12 @@ class CalcTableViewController: UITableViewController, ElecPopoverTableViewContro
         remainingCalcItems = allCalcItems
         
         addButton.enabled = true
+        
+        brain.calculateFinished = false
     }
-
+    
+    func spookyScarySkeleton()
+    {
+        print("  ▒▒▒░░░░░░░░░░▄▐░░░░ \n ▒░░░░░░▄▄▄░░▄██▄░░░ \n ░░░░░░▐▀█▀▌░░░░▀█▄░ \n ░░░░░░▐█▄█▌░░░░░░▀█▄ \n ░░░░░░░▀▄▀░░░▄▄▄▄▄▀▀ \n ░░░░░▄▄▄██▀▀▀▀░░░░░ \n ░░░░█▀▄▄▄█░▀▀░░░░░░ \n ░░░░▌░▄▄▄▐▌▀▀▀░░░░░ \n ░░░░▌░▄▄▄▐▌▀▀▀░░░░░ \n ░▄░▐░░░▄▄░█░▀▀░░░░░ \n ░▀█▌░░░▄░▀█▀░▀░░░░░ \n ░░░░░░░░▄▄▐▌▄▄░░░░░ \n ░░░░░░░░▀███▀█░▄░░░ \n ░░░░░░░▐▌▀▄▀▄▀▐▄░░░ \n ░░░░░░░▐▀░░░░░░▐▌░░ \n ░░░░░░░█░░░░░░░░█░░ \n ░░░░░░▐▌░░░░░░░░░█░  ")
+    }
 }
