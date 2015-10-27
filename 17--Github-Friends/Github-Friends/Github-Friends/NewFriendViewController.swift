51a761086c1a5fb278d79deb2378e022c5c3fce8
daaavid <david.j.c.johnson@gmail.com>
2015-10-27 13:51:55 -0400

    Initial commit

---
+++ 17--Github-Friends/Github-Friends/Github-Friends/NewFriendViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Github-Friends
+//
+//  Created by david on 10/27/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class NewFriendViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
