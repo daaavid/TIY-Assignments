d8558ef5403e65e597e70d5d83140441cb3a500b
daaavid <david.j.c.johnson@gmail.com>
2015-10-29 09:21:29 -0400

    --

--- Coding Kata/Kata2.playground/Contents.swift
+++ Coding Kata/Kata2.playground/Contents.swift
@@ -3,16 +3,12 @@
 //: You are given a year, determine if it’s a leap year. A leap year is a year containing an extra day. It has 366 days instead of the normal 365 days. The extra day is added in February, which has 29 days instead of the normal 28 days. Leap years occur every 4 years. 2012 is a leap year and so is 2016. Except that every 100 years special rules apply. Years that are divisible by 100 are not leap years if they are not divisible by 400. For example 1900 was not a leap year, but 2000 was. __Print Leap year! or Not a leap year! depending on the case.__
 func checkLeapYear(year: Int) -> Bool
 {
-    var rc = true
+    var rc = false
     if year % 100 == 0// && year % 400 == 0
     {
         if year % 400 == 0
         {
-            return rc
-        }
-        else
-        {
-            rc = false
+            rc = true
         }
     }
     return rc
@@ -23,6 +19,7 @@ checkLeapYear(2016)
 checkLeapYear(2020)
 checkLeapYear(1900)
 checkLeapYear(2000)
+checkLeapYear(1999)
 //: ## Question 2
 //: ### Food Spoilage
 //: You are working on a smart-fridge. The smart-fridge knows how old the eggs and bacon in it are. You know that eggs spoils after 3 weeks (21 days) and bacon after one week (7 days). Given ```baconAge``` and ```eggsAge``` (in days) determine if you can cook bacon and eggs or what ingredients you need to throw out.
@@ -74,7 +71,7 @@ var sum = 0
 
 var sumOfSq = 0
 
-while num <= 10
+while num <= 100
 {
     var numSq = num * num
     num++
@@ -84,7 +81,7 @@ while num <= 10
 
 sumOfSq
 
-for var x = 0; x <= 10; x++
+for var x = 0; x <= 100; x++
 {
     sum = sum + x
 }
