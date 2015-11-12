7bc76bfab4403dbf7804e9b4469cc02705698371
Keron Williams <keronwilliams9@gmail.com>
2015-11-12 14:53:06 -0500

    + adding detailVC Api

--- 29--Movie-Mania/Movie Mania/Movie Mania/DetailViewController.m
+++ 29--Movie-Mania/Movie Mania/Movie Mania/DetailViewController.m
@@ -8,7 +8,14 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController ()
+@interface DetailViewController () <NSURLSessionDelegate>
+{
+    NSDictionary *searchResults;
+    NSMutableData *receivedData;
+    NSURLSessionDataTask *task;
+
+}
+
 @property (weak, nonatomic) IBOutlet UILabel *testLabel;
 
 @end
@@ -20,6 +27,9 @@
     
     self.testLabel.text = self.selectedMovieDictionary[@"Title"];
     // Do any additional setup after loading the view.
+    
+    [self search];
+    searchResults = [[NSDictionary alloc]init];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,6 +37,46 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(void)search
+{
+    NSString *searchTerm = self.selectedMovieDictionary[@"Title"];
+    NSString *urlString = [NSString stringWithFormat:@"https://www.omdbapi.com/?t=%@&y=&plot=short&r=json", searchTerm];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:[NSOperationQueue mainQueue]];
+    
+    task = [session dataTaskWithURL:url];
+    [task resume];
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler
+{
+    completionHandler(NSURLSessionResponseAllow);
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data
+{
+    if (!receivedData) //if haven't received data
+    {
+        receivedData = [[NSMutableData alloc] init];
+        //initializing thing to store data
+    }
+    [receivedData appendData: data];
+}
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
+{
+    //received all data or received error
+    if (!error) //if there was no error
+    {
+        NSLog(@"Download success");
+        searchResults = [NSJSONSerialization JSONObjectWithData:receivedData options:0 error:nil];
+        self.testLabel.text =  (NSString *)searchResults[@"Director"];
+        
+       
+    }
+}
 /*
 #pragma mark - Navigation
 
